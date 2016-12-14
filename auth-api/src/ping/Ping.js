e7998c61195f5bf48f84585abfd2316949f705c6
Jeremy Thomerson <jeremy@thomersonfamily.com>
2016-12-14 23:27:25 +0300

    Stub of a serverless API for auth

---
+++ auth-api/src/ping/Ping.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Handler;
+
+require('dotenv').config({ silent: true });
+
+Handler = {
+
+   handler: function(evt, context, cb) {
+      var data, appName;
+
+      data = {
+         response: 'pong',
+         time: new Date().getTime(),
+      };
+
+      appName = process.env.SERVERLESS_PROJECT;
+      appName = appName + ':' + process.env.SERVERLESS_SERVICE_NAME;
+      appName = appName + ':' + process.env.SERVERLESS_STAGE;
+
+      cb(null, {
+         statusCode: 200,
+         headers: {
+            'Access-Control-Allow-Origin': '*',
+            'X-App': appName,
+         },
+         body: JSON.stringify({
+            response: 'pong',
+            time: new Date().getTime(),
+         }),
+      });
+   },
+
+};
+
+module.exports = Handler;
